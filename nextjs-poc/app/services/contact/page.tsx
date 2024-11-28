66c67c04003785ccc9a05bd893d5908830c1f61d
Hugo <hugo.duchemin@epitech.eu>
2024-11-28 09:46:15 +0100

    chore: get rid of node_modules dir

--- nextjs-poc/app/services/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-    return (
-        <div className="flex flex-col items-center justify-center mt-16">
-            <h1 className="mt-16 font-semibold text-3xl"> Une proposition de nouvelles fonctionnalité ? </h1>
-            <h2 className="font-semibold p-4 text-3xl"> Un retour à faire sur notre site ? </h2>
-            <p  className="font-medium text-black text-xl" > Contactez nous à : area-project@noj.fr</p>
-        </div>
-    );
-}
---
+++ web/app/services/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+import {Button} from "@/components/ui/button";
+import {redirect} from "next/navigation";
+
+export default function Page() {
+    return (
+        <div>
+            <h1> Hola </h1>
+        </div>
+    );
+}
