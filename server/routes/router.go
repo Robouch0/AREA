8d5ee43058e3123245945aa42e6e67057193efcb
rahul.chander <rahul.chander@epitech.eu>
2024-11-28 17:14:18 +0100

    feat: Separating routes per file

--- server/routes/router.go
+++ server/routes/router.go
@@ -8,29 +8,9 @@
 package routes
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 )
-
-type User struct {
-	ID   string `json:"id"`
-	NAME string `json:"name"`
-}
-
-func UserRoutes() chi.Router {
-	userRouter := chi.NewRouter()
-
-	userRouter.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		err := json.NewEncoder(w).Encode(User{ID: "1", NAME: "Pahul"})
-		if err == nil {
-			return
-		}
-	})
-	return userRouter
-}
 
 func InitHTTPServer() chi.Router {
 	r := chi.NewRouter()
