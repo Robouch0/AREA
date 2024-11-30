d32d68e48267b11b644eb7b0456cfadb7e7aef16
Hugo <hugo.duchemin@epitech.eu>
2024-11-30 23:31:00 +0100

    feat: added the bg image on the contact page

--- web/app/services/contact/page.tsx
+++ web/app/services/contact/page.tsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 export default function Page() {
     return (
         <div className="flex flex-col items-center justify-center mt-16">
             <h1 className="mt-16 font-semibold text-3xl text-white"> Une proposition de nouvelles fonctionnalité ? </h1>
             <h2 className="font-semibold p-4 text-3xl text-white"> Un retour à faire sur notre site ? </h2>
             <p  className="font-medium text-white text-xl" > Contactez nous à : area-project@noj.fr</p>
+            <Image
+                className="my-12 opacity-60"
+                src="/bg.png"
+                alt="bg Social Networks"
+                width="320"
+                height="320"
+            />
         </div>
     );
 }
