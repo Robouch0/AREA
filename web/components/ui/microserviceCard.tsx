a0cf658da7407d2b13bb75e0e83b91c9e8a903d4
Hugo Duchemin <114905866+Robouch0@users.noreply.github.com>
2024-12-02 11:15:59 +0100

    49 services pages to see the default area (#54)
    * feat: added the services explore page, the customCard to display Area, a basic router logic to redirect to a Create page
    
    * feat: upgraded the layout background and the card display on services page. Also added a searhbar inputfield
    
    * feat: updated navabar layout with two redirect buttons
    
    * feat: first implementation of the create page
    
    * feat: added reaction front basic handling, and fix some responsiveness problem on the navbar
    
    * feat: added intro to the website
    
    * feat: added the bg image for home page, and added a register page frontend
    
    * feat: added the bg image on the contact page
    
    * feat: now retrieve the value in the input field for login
    
    * chore: fix ci dependency problems
    
    * style: added type for ts linter

---
+++ web/components/ui/microserviceCard.tsx
@@ -0,0 +1,26 @@
+'use client';
+import {Card, CardHeader, CardTitle, CardDescription} from "@/components/ui/card";
+import {Button} from "@/components/ui/button";
+export function MicroServiceCard({microServicesColor, title, description, setId, id} : {microServicesColor:string|undefined; title:string, description:string, setId:React.Dispatch<React.SetStateAction<number>>; id:number}) {
+
+    return (
+        <>
+            <Button
+                className="hover:bg-transparent shadow-none bg-transparent my-2 w-80 h-64"
+                onClick={() => setId(id)}
+            >
+                <Card
+                    className={"font-black text-3xl w-full h-full border-none hover:opacity-75 items-center text-left py-2 flex flex-col justify-between"}
+                    style={{backgroundColor: microServicesColor}}
+                >
+                    <CardHeader className="text-wrap">
+                        <CardTitle className="my-2 !text-2xl break-words text-white">{title}</CardTitle>
+                    </CardHeader>
+                    <CardDescription className="px-6 py-4 text-black text-xl text-wrap">
+                        {description}
+                    </CardDescription>
+                </Card>
+            </Button>
+        </>
+    );
+}
