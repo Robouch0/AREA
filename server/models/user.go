194fefa49ab45e0acc89def5c4eb736e8d4ee3ca
mattgod56 <matthieu.godme@epitech.eu>
2024-12-03 12:15:09 +0100

    feat: modified db to store tokens

--- server/models/user.go
+++ server/models/user.go
@@ -26,3 +26,11 @@ type User struct {
 	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"-"`
 	UpdatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"-"`
 }
+
+type OAuthToken struct {
+	ID           int64     `bun:"id,pk,autoincrement" json:"id,pk,autoincrement"`
+	UserID       int64     `bun:"user_id" json:"user_id"`
+	Provider     string    `bun:"provider" json:"provider"`
+	AccessToken  string    `bun:"access_token" json:"access_token"`
+	ExpiresAt    time.Time `bun:",nullzero,notnull,default:current_timestamp" json:"-"`
+}
