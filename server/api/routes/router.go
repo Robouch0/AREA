608127f2553be91a687a5dd7f6ae701454d8f1fa
rahul.chander <rahul.chander@epitech.eu>
2024-12-11 18:55:32 +0100

    feat: Docker compose for golang server app

--- server/api/routes/router.go
+++ server/api/routes/router.go
@@ -44,15 +44,6 @@ func InitHTTPServer() (*api.ApiGateway, error) {
 		MaxAge:           300,
 	}))
 
-	gateway.Router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300,
-	}))
-
 	gateway.Router.Get("/swagger/*", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:3000/swagger/doc.json"),
 	))
