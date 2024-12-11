3bca76e473af5785772a7921ac8d3fde8880320a
rahul.chander <rahul.chander@epitech.eu>
2024-12-11 20:19:56 +0100

    feat: Docker compose for web app and internal and external networking

--- web/lib/axios.ts
+++ web/lib/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: process.env.GATEWAY_URL
+    // baseURL: 'http://localhost:3000/'
 });
 
 export default axiosInstance;
