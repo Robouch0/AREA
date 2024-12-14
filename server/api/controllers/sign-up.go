269a7042f67681208fbcce041840209c85432131
rahul.chander <rahul.chander@epitech.eu>
2024-12-14 21:29:04 +0100

    feat: New response bodies for login and signup

--- server/api/controllers/sign-up.go
+++ server/api/controllers/sign-up.go
@@ -10,11 +10,20 @@ package controllers
 import (
 	"area/db"
 	"area/models"
+	"area/utils"
 	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 )
+
+type userSignUp struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
 
 // Account godoc
 // @Summary      Sign-up a new account
@@ -23,17 +32,17 @@ import (
 // @Accept       json
 // @Produce      json
 // @Param 		 newUser body	models.User	true 	"New User informations to sign-up to the app"
-// @Success      200  {object}  models.User
+// @Success      200  {object}  userSignUp
 // @Failure      401  {object}  error
 // @Failure      500  {object}  error
 // @Router       /sign-up/ [post]
 func SignUp(w http.ResponseWriter, r *http.Request) {
-	var newUser models.User
+	var newUser userSignUp
 
 	err := json.NewDecoder(r.Body).Decode(&newUser)
 	if err != nil {
-		w.WriteHeader(401)
-		fmt.Printf("Json Error: %v\n", err)
+		utils.WriteHTTPResponseErr(&w, 401, "Invalid request body")
+		log.Println(err)
 		return
 	}
 
@@ -44,23 +53,27 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 		Where("email = ?", us.Email).
 		Scan(context.Background())
 	if err == nil {
-		w.WriteHeader(401)
-		w.Write([]byte("An user already exists with this email address"))
+		utils.WriteHTTPResponseErr(&w, 401, "An user already exists with this email address")
+		log.Println(err)
 		return
 	}
 
-	res, err := userDb.CreateUser(&newUser)
+	res, err := userDb.CreateUser(&models.User{
+		FirstName: newUser.FirstName,
+		LastName:  newUser.LastName,
+		Email:     newUser.Email,
+		Password:  newUser.Password,
+	})
 	if err != nil {
-		w.WriteHeader(500)
-		fmt.Println(err)
-		w.Write([]byte("Error while creating an user"))
+		utils.WriteHTTPResponseErr(&w, 500, "Error while creating an user")
+		log.Println(err)
 		return
 	}
 
 	b, err := json.Marshal(res)
 	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte("Error while converting results to json"))
+		utils.WriteHTTPResponseErr(&w, 500, "Error while converting results to json")
+		log.Println(err)
 		return
 	}
 	w.WriteHeader(200)
