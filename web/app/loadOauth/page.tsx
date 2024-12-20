667ae2f65006a5fc5c5186b50fcd981eef7c0e5d
Hugo <hugo.duchemin@epitech.eu>
2024-12-20 10:32:01 +0100

    feat: base code for window popup needed in Oauth

---
+++ web/app/loadOauth/page.tsx
@@ -0,0 +1,5 @@
+"use server";
+export default async function LoadOauth() {
+
+    return <div className="text-7xl text-red-600"> Hola </div>;
+}
--- web/components/ui/services/OauthButton.tsx
+++ web/components/ui/services/OauthButton.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import { useRouter } from "next/navigation";
 import axiosInstance from "@/lib/axios";
 import { oauhLogin } from "@/api/authentification";
@@ -17,12 +17,12 @@ async function redirectToService(service: string) {
                 "redirect_uri": "http://127.0.0.1:8081"
             }
         });
-        window.location.href = response.data;
+        return response.data;
     } catch (error) {
         console.error(error);
     }
 }
-
+// http://127.0.0.1:8081
 async function askForToken(service: string, code: string | null) {
     try {
         await oauhLogin({ service: service, code: code, redirect_uri: "http://127.0.0.1:8081" })
@@ -36,6 +36,49 @@ async function askForToken(service: string, code: string | null) {
 export function OauthButton({ service, className, ServiceIcon }: IOAuthButton) {
     const serviceDisplayName = service.charAt(0).toUpperCase() + service.slice(1);
     const router = useRouter();
+    const [popupWindow, setPopupWindow] = useState<Window|null>(null);
+    const [code, setPopupCode] = useState<string|null>("");
+
+
+    const openPopup = useCallback(async (service) => {
+        const url:string = await redirectToService(service)
+
+            if (typeof window !== 'undefined') {
+                const newWindow = window.open(url, "popup", "width=900,height=700,left=400,top=700,popup=true");
+                setPopupWindow(newWindow);
+            }
+    }, []);
+
+
+    useEffect(() => {
+        if (!popupWindow) {
+            return;
+        }
+
+        const checkPopup = setInterval(() => {
+            if (popupWindow.closed) {
+                clearInterval(checkPopup);
+                setPopupWindow(null);
+                return;
+            }
+
+            try {
+                console.log(popupWindow.location.href);
+                if (popupWindow.location.href.includes("code")) {
+                    const url = new URL(popupWindow.location.href);
+                    const code: string|null = url.searchParams.get('code');
+                    window.opener.postMessage({ code: `${code}` }, 'http://127.0.0.1:8081');
+
+                    popupWindow.close();
+                    clearInterval(checkPopup);
+                    setPopupWindow(null);
+                }
+            } catch (e) {
+                console.error("error:", e);
+            }
+        }, 1000);
+
+    }, [popupWindow]);
 
     useEffect(() => {
         const url = new URL(window.location.href);
@@ -52,7 +95,7 @@ export function OauthButton({ service, className, ServiceIcon }: IOAuthButton) {
     return (
         <Button
             className={className}
-            onClick={() => { redirectToService(service) }}
+            onClick={() => { openPopup(service) }}
         >
             {ServiceIcon}
             <p className="mx-3 text-2xl font-semibold">Continuer avec {serviceDisplayName}</p>
