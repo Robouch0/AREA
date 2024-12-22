e488e45ae2e7c3f57371b4b7f567e795a594cc72
Astro. <mattgod56@gmail.com>
2024-12-22 21:30:01 +0100

    fix(): unused parameter

--- web/components/pages/login/LoginForm.tsx
+++ web/components/pages/login/LoginForm.tsx
@@ -4,7 +4,7 @@ import { FormData, UserSchema, ValidFieldNames } from "@/lib/typeLogin";
 import FormField from "@/components/ui/utils/FormField";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
-import { FaDiscord, FaEye, FaEyeSlash, FaFacebook, FaGoogle } from "react-icons/fa";
+import { FaDiscord, FaEye, FaEyeSlash, FaFacebook } from "react-icons/fa";
 import { useEffect, useState } from "react";
 import { login } from "@/api/authentification";
 import { useRouter } from "next/navigation";
