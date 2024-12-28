a6773cb7b2f1ff253180808ad7f165edf091668e
Astro. <mattgod56@gmail.com>
2024-12-28 17:19:21 +0100

    feat(): structures to handle github webhooks

--- server/gRPC/api/github/githubTypes/githubWebhookRequest.go
+++ server/gRPC/api/github/githubTypes/githubWebhookRequest.go
@@ -7,3 +7,12 @@
 
 package githubtypes
 
+type GithubConfig struct {
+	Url     string `json:"url"`
+	Content string `json:"content_type"`
+}
+
+type GitWebHookRequest struct {
+	Event  []string       `json:"events"`
+	Config GithubConfig `json:"config"`
+}
