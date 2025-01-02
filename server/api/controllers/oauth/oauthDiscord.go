a19f12b3a4b32b3fe17022f37d0901f9dfd91aa9
Hugo <hugo.duchemin@epitech.eu>
2025-01-02 15:17:32 +0100

    style: fix ts style error, causing the ci to fail, and a typo error on the go server

--- server/api/controllers/oauth/oauthDiscord.go
+++ server/api/controllers/oauth/oauthDiscord.go
@@ -44,7 +44,7 @@ func (discord *DiscordOAuth) GetAccessToken(OAuthCode *OAuthRequest) (*OAuthAcce
 	values.Set("code", OAuthCode.Code)
 	values.Set("redirect_uri", OAuthCode.RedirectURI)
 
-q	response, resperr := http.PostForm(discord.AccessTokenURL, values)
+	response, resperr := http.PostForm(discord.AccessTokenURL, values)
 
 	if resperr != nil {
 		return nil, resperr
