c55eca521c97142432b8cdcb983eb8d40202098a
Axel Tacheau <axel.tacheau@epitech.eu>
2025-01-02 04:38:02 +0100

    feat(mobile): Implement the new navigation bar widget to the app and link it to the gorouter service

--- mobile/lib/core/router/app_router.dart
+++ mobile/lib/core/router/app_router.dart
@@ -9,23 +9,22 @@ import 'package:my_area_flutter/services/api/auth_service.dart';
 import 'package:my_area_flutter/pages/login_page.dart';
 import 'package:my_area_flutter/pages/home_page.dart';
 import 'package:my_area_flutter/pages/page_not_found.dart';
+import 'package:my_area_flutter/widgets/bottom_navbar.dart';
 
 import 'route_names.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
-    initialLocation: RouteNames.create,
+    initialLocation: RouteNames.home,
     redirect: (context, state) {
       final authService = AuthService.instance;
       final isLoggedIn = authService.isLoggedInSync;
       final isLoginPage = state.matchedLocation == RouteNames.login;
       final isSignupPage = state.matchedLocation == RouteNames.signup;
+      final isAuthPage = isLoginPage || isSignupPage;
 
-      if (!isLoggedIn && !isLoginPage && !isSignupPage) {
+      if (!isLoggedIn && isAuthPage) {
         return RouteNames.login;
-      }
-      if (isLoggedIn && (isLoginPage || isSignupPage)) {
-        return RouteNames.home;
       }
       return null;
     },
@@ -35,25 +34,38 @@ class AppRouter {
         builder: (context, state) => const LoginPage(),
       ),
       GoRoute(
-        path: RouteNames.home,
-        builder: (context, state) => const HomePage(),
-      ),
-      GoRoute(
         path: RouteNames.signup,
         builder: (context, state) => const RegisterPage(),
       ),
-      GoRoute(
-        path: RouteNames.create,
-        builder: (context, state) => CreateAreaPage(services: AreaService.instance.listAreas(), uid: 12345),
+      ShellRoute(
+        builder: (context, state, child) {
+          final isLoginPage = state.matchedLocation == RouteNames.login;
+          final isSignupPage = state.matchedLocation == RouteNames.signup;
+          final isAuthPage = isLoginPage || isSignupPage;
+          if (isAuthPage) {
+            return child;
+          }
+          return BottomNavbar(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: RouteNames.home,
+            builder: (context, state) => const HomePage(),
+          ),
+          GoRoute(
+            path: RouteNames.create,
+            builder: (context, state) => CreateAreaPage(services: AreaService.instance.listAreas(), uid: 12345),
+          ),
+          GoRoute(
+              path: RouteNames.profile,
+              builder: (context, state) => const ProfilePage(
+                  email: 'axeltacheau@live.fr',
+                  firstName: 'Axel',
+                  lastName: 'Tacheau',
+                  password: 'axel1208')
+          )
+        ],
       ),
-      GoRoute(
-        path: RouteNames.profile,
-        builder: (context, state) => const ProfilePage(
-            email: 'axeltacheau@live.fr',
-            firstName: 'Axel',
-            lastName: 'Tacheau',
-            password: 'axel1208')
-      )
     ],
     errorBuilder: (context, state) => const NotFoundPage(),
   );
