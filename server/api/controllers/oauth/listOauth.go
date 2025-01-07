b7721ce1a71e41896944daf2c40ca1d065d07f7c
Astro. <mattgod56@gmail.com>
2025-01-07 14:05:24 +0100

    feat(): add code to route + content type

--- server/api/controllers/oauth/listOauth.go
+++ server/api/controllers/oauth/listOauth.go
@@ -12,12 +12,22 @@ import (
 	"net/http"
 )
 
+// Sign-up OAuth godoc
+// @Summary      List Oauth
+// @Description  List all the current Oauth handled by the server
+// @Tags         Account
+// @Accept       json
+// @Produce      json
+// @Success      200  {object}  []string
+// @Router       /oauth/oauthlist [get]
 func ListOauth(OAuthURLs map[string]OAuthURLs) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var Oauths []string
 		for key, _ := range OAuthURLs {
 			Oauths = append(Oauths, key)
 		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
 		json.NewEncoder(w).Encode(Oauths)
 	}
 }
