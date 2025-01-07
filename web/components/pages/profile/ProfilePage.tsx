1870f3a7f2d2d06923bacbf93180a239b67875c6
Hugo <hugo.duchemin@epitech.eu>
2025-01-07 12:52:15 +0100

    feat: toaster for when db is down in the profile page

--- web/components/pages/profile/ProfilePage.tsx
+++ web/components/pages/profile/ProfilePage.tsx
@@ -16,7 +16,7 @@ import {useToast} from "@/hooks/use-toast";
 
 
 
-export default function ProfilePage({email, first_name, last_name, password, providers}: userInfo) {
+export default function ProfilePage({id, email, first_name, last_name, password, providers}: userInfo) {
     const [showPassword, setShowPassword] = useState(false);
     const [firstName, setFirstName] = useState(first_name);
     const [lastName, setLastName] = useState(last_name);
@@ -37,6 +37,7 @@ export default function ProfilePage({email, first_name, last_name, password, pro
     function handleDisconnectProvider(provider: string) {
         unlinkOauthProvider(provider);
     }
+
     function handleDataUpdate() {
         updateUserInfos(firstName, lastName, passw).then(() => {
         toast({
@@ -57,6 +58,16 @@ export default function ProfilePage({email, first_name, last_name, password, pro
 
     const reloadPage = () => {
         window.location.reload()
+    }
+
+    if (id == -1) {
+        toast({
+            title: "The area server is down",
+            description: "Cannot get your profile datas for the moment",
+            variant: 'destructive',
+            duration: 2500,
+        })
+        return <> </>;
     }
 
     return (
