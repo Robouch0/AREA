f378b9fd665bf23bebeb24eede92cdd4835d04fc
Astro. <mattgod56@gmail.com>
2025-01-10 11:15:07 +0100

    fix(): change branch for pull request

--- server/gRPC/api/github/githubTypes/githubWebhookResponse.go
+++ server/gRPC/api/github/githubTypes/githubWebhookResponse.go
@@ -14,5 +14,5 @@ type GithubEvents struct {
 type GithubWebHookResponse struct {
 	Events []string `json:"events"`
 	Id     int64    `json:"id"`
-	Action string 	`json:"action,omitempty"`
+	Action string   `json:"action,omitempty"`
 }
