9abde6d4a1a5d50fd76dddb6d48c484ac6a3b331
Astro. <mattgod56@gmail.com>
2025-01-10 16:53:02 +0100

    feat(): new protofile & files for cryptocompare service

---
+++ server/gRPC/api/cryptoCompare/cryptoCompareClient/cryptoActions.go
@@ -0,0 +1,8 @@
+//
+// EPITECH PROJECT, 2025
+// AREA [WSL: Ubuntu]
+// File description:
+// cryptoActions
+//
+
+package cryptocompareclient
---
+++ server/gRPC/api/cryptoCompare/cryptoCompareClient/cryptoClient.go
@@ -0,0 +1,8 @@
+//
+// EPITECH PROJECT, 2025
+// AREA [WSL: Ubuntu]
+// File description:
+// cryptoClient
+//
+
+package cryptocompareclient
