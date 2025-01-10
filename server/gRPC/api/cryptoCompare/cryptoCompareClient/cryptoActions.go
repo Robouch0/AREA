8dd2633db166ba5bca8a6741712a9e7b726023b3
Astro. <mattgod56@gmail.com>
2025-01-10 17:14:30 +0100

    feat(): all microservice function client side

--- server/gRPC/api/cryptoCompare/cryptoCompareClient/cryptoActions.go
+++ server/gRPC/api/cryptoCompare/cryptoCompareClient/cryptoActions.go
@@ -6,3 +6,61 @@
 //
 
 package cryptocompareclient
+
+import (
+	IServ "area/gRPC/api/serviceInterface"
+	"area/models"
+	gRPCService "area/protogen/gRPC/proto"
+	grpcutils "area/utils/grpcUtils"
+	"encoding/json"
+)
+
+func (crypto *CryptoClient) SendIsHigherAction(scenario models.AreaScenario, actionID, userID int) (*IServ.ActionResponseStatus, error) {
+	wReqBytes, err := json.Marshal(scenario.Action.Ingredients)
+	if err != nil {
+		return nil, err
+	}
+
+	wRequest := gRPCService.IsHigherThanTriggerReq{
+		Activated: true,
+		ActionId:  int32(actionID),
+	}
+	err = json.Unmarshal(wReqBytes, &wRequest)
+	if err != nil {
+		return nil, err
+	}
+	ctx := grpcutils.CreateContextFromUserID(userID)
+	_, err = crypto.cc.IsHigherThanTrigger(ctx, &wRequest)
+	if err != nil {
+		return nil, err
+	}
+	return &IServ.ActionResponseStatus{
+		Description: "Done",
+		ActionID:    actionID,
+	}, nil
+}
+
+func (crypto *CryptoClient) SendIsLowerAction(scenario models.AreaScenario, actionID, userID int) (*IServ.ActionResponseStatus, error) {
+	wReqBytes, err := json.Marshal(scenario.Action.Ingredients)
+	if err != nil {
+		return nil, err
+	}
+
+	wRequest := gRPCService.IsLowerThanTriggerReq{
+		Activated: true,
+		ActionId:  int32(actionID),
+	}
+	err = json.Unmarshal(wReqBytes, &wRequest)
+	if err != nil {
+		return nil, err
+	}
+	ctx := grpcutils.CreateContextFromUserID(userID)
+	_, err = crypto.cc.IsLowerThanTrigger(ctx, &wRequest)
+	if err != nil {
+		return nil, err
+	}
+	return &IServ.ActionResponseStatus{
+		Description: "Done",
+		ActionID:    actionID,
+	}, nil
+}
