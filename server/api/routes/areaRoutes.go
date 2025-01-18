f819b1027be1613256ecd40b6057bda1e34472da
rahul.chander <rahul.chander@epitech.eu>
2025-01-18 15:29:48 +0100

    fix: Useless comment

--- server/api/routes/areaRoutes.go
+++ server/api/routes/areaRoutes.go
@@ -19,7 +19,7 @@ func AreaRoutes(gateway *api.ApiGateway) chi.Router {
 	areaRouter := chi.NewRouter()
 	areaDb := db.GetAreaDb()
 
-	areaRouter.Get("/list", areas.GetUserAreas(gateway, areaDb)) // I don't know if more details are necessary
+	areaRouter.Get("/list", areas.GetUserAreas(gateway, areaDb))
 	areaRouter.Put("/activate", areas.ActivateArea(gateway))
 	areaRouter.Post("/create/{service}", areas.CreateArea(gateway))
 	areaRouter.Get("/create/list", areas.ListService(gateway))
